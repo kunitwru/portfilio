f6e9d1e74abb49345cbe6332794545103099eb9b
Pham Sinh <sinhpv86@gmail.com>
2018-10-17 16:36:14 +0700

    Update UI

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// resume
+router.get('/resume.html', function (req, res, next) {
+    res.render('resume/index');
+});
+
+// resume
+router.get('/portfolio.html', function (req, res, next) {
+    res.render('portfolio/index');
+});
+
+router.get('/blog.html', function (req, res, next) {
+    res.render('blog/index');
+});
+
+router.get('/contact.html', function (req, res, next) {
+    res.render('contact/index');
+})
+
 module.exports = router;
